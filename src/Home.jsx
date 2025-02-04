4e10b92457be9a13b871aad95db81e5caea1ac93
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-04 23:42:19 +0530

    Add header

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <header>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+        </header>
+    </>
+  )
+}
+
+export default Home
