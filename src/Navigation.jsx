ac337f98e4b76b91a394a8ee6ab8c3c896cf979d
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-04 21:16:41 +0530

    Add navigation items array

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,11 +1,63 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const navigationLinks = ["Home", "About", "Menu", "Reservations", "order online", "Login"]
+"Home", "About", "Menu", "Reservations", "order online", "Login"
+
+const navigationLinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/"
+  },
+
+  {
+    id: 2,
+    name: "About",
+    link: "/about"
+  },
+
+  {
+    id: 3,
+    name: "Menu",
+    link: "/menu"
+  },
+
+  {
+    id: 4,
+    name: "Reservations",
+    link: "/reservations"
+  },
+
+  {
+    id: 5,
+    name: "Order Online",
+    link: "/order-online"
+  },
+
+  {
+    id: 6,
+    name: "Login",
+    link: "/login"
+  }
+];
+
+const NavigationItems =() => {
+
+    return(
+
+      <li>
+        <NavLink to="/" exact activeClassName="active">Home</NavLink>
+      </li>
+
+    )
+}
 
 const Navigation = () => {
   return (
     <nav>
-        
+        <ul>
+
+        </ul>
     </nav>
   )
 }
