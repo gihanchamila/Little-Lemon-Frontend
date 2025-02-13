0739b0e750f31b882fadaca41764b25615059a1c
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-13 14:50:46 +0530

    changed base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/little-lemon/",
+  base:"/Little-Lemon/",
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
