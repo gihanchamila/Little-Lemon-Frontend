412faff1b33ef967effa48882776bffad450d653
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-14 19:26:28 +0530

    add outline styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 const Button = ({ type = 'primary', className = '', outline = false, children, ...buttonProps }) => {
   const baseClassName = type === 'primary' ? 'primaryButton' : 'secondaryButton';
-  const outlineClassName = outline ? 'outlineButton' : '';
+  const outlineClassName = outline ? 'outline' : '';
 
-  
-  
   const combinedClassName = `button ${baseClassName} ${outlineClassName} ${className}`.trim();
 
   return (
-    <button role="button" className={combinedClassName} {...buttonProps}>
+    <button role="button" className={outline ?outlineClassName : combinedClassName} {...buttonProps}>
       {children}
     </button>
   );
